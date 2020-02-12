9a851f8a07208ab6d6dbfb3df5c40e24ff5f3330
MWarCZ <mv.deoxys@gmail.com>
2020-02-12 11:38:09 +0100

    Pridany a upraveny funkce pro vytvareni kontextu.

--- src/bpmnRunner/runContext.ts
+++ src/bpmnRunner/runContext.ts
@@ -1,27 +1,53 @@
 import { Connection, Equal, In } from 'typeorm'
 
-import { DataObjectInstance, DataObjectTemplate, TaskInstance } from '../entity/bpmn'
+import { ActivityStatus, DataObjectInstance, DataObjectTemplate, TaskInstance, TaskTemplate } from '../entity/bpmn'
 
+////////////////////////////
+// createXXX - synchroni funkce, ktere nepracuji primo s databazi.
+// loadXXX - asynchroni funkce, ktere pracuji primo s databozi.
+//
 
-
-type RunContextInput = {
-  [dataObjectName: string]: any,
+export type RunContextMap = {
+  [key: string]: any
+}
+export type RunContextInput = RunContextMap
+export type RunContextOutput = RunContextMap
+export type RunContextTask = {
+  // Z instance
+  startDate: Date,
+  endData: Date,
+  status: ActivityStatus,
+  // Ze sablony
+  name: string,
+  bpmnId: string,
+  implementation: string,
 }
 
 export type RunContext = {
   $GLOBAL: any,
   $INPUT: RunContextInput,
+  $OUTPUT: RunContextOutput,
+  $SELF: Partial<RunContextTask>,
+}
+
+export function createEmptyContext(): RunContext {
+  return {
+    $GLOBAL: {},
+    $INPUT: {},
+    $OUTPUT: {},
+    $SELF: {},
+  }
 }
 
 export function createContextInputs(
   options: {
-    taskInputsTemplates: DataObjectTemplate[],
-    taskInputsInstances: DataObjectInstance[],
+    inputsDataTemplates: DataObjectTemplate[],
+    inputsDataInstances: DataObjectInstance[],
   },
 ): RunContextInput {
   const {
-    taskInputsTemplates,
-    taskInputsInstances,
+    inputsDataTemplates: taskInputsTemplates,
+    inputsDataInstances: taskInputsInstances,
   } = options
 
   let data: RunContextInput  = taskInputsTemplates.map(inputTemplate => {
@@ -41,27 +67,87 @@ export function createContextInputs(
   return data
 }
 
+export function createContextOutputs(
+  options: {
+    outputsDataTemplates: DataObjectTemplate[],
+    outputsDataInstances: DataObjectInstance[],
+  },
+): RunContextOutput {
+  const {
+    outputsDataTemplates: taskOutputsTemplates,
+    outputsDataInstances: taskOutputsInstances,
+  } = options
+  let data = createContextInputs({
+    inputsDataTemplates: taskOutputsTemplates,
+    inputsDataInstances: taskOutputsInstances,
+  })
+  return data
+}
 
+export function createContextForTask(
+  options: {
+    taskTemplate: TaskTemplate,
+    taskInstance: TaskInstance,
+    inputsDataTemplates: DataObjectTemplate[],
+    inputsDataInstances: DataObjectInstance[],
+    outputsDataTemplates: DataObjectTemplate[],
+    outputsDataInstances: DataObjectInstance[],
+    context?: RunContext,
+  },
+): RunContext {
+  const {
+  	taskTemplate,
+    taskInstance,
+    inputsDataTemplates,
+    inputsDataInstances,
+    outputsDataTemplates,
+    outputsDataInstances,
+    context = createEmptyContext(),
+  } = options
 
-export async function createContext(
+  context.$SELF = {
+    // Z instance
+    startDate: undefined,
+    endData: undefined,
+    status: taskInstance.status,
+    // Ze sablony
+    bpmnId: taskTemplate.bpmnId,
+    name: taskTemplate.name,
+    implementation: taskTemplate.implementation,
+  }
+
+  let inputsData = createContextInputs({
+    inputsDataInstances,
+    inputsDataTemplates
+  })
+  context.$INPUT = { ...context.$INPUT, ...inputsData}
+
+  let outputsData = createContextOutputs({
+    outputsDataInstances,
+    outputsDataTemplates
+  })
+  context.$OUTPUT = { ...context.$OUTPUT, ...outputsData}
+
+  return context
+}
+
+export async function loadContextForTask(
   taskInstance: { id: number },
   typeormConnection: Connection,
 ) {
-  let context: RunContext = {
-    $GLOBAL: {},
-    $INPUT: {},
-  }
   // [x] Ziskat instanci ulohy.
   // [x] Ziskat sablonu ulohy.
   // [x] Ziskat datove vstupy dane sablony ulohy.
   // [x] Ziskat existujici instance datovych vstupu.
   //    [x] Stejna instance procesu pro instanci ulohy a instance datoveho objektu.
   //    [x] Instance datoveho objektu je vytvorena dle sablon datovych vstupu sablony ulohy.
+  //
+  let context: RunContext = createEmptyContext()
 
-  let taskI = await typeormConnection.getRepository(TaskInstance).findOneOrFail({
+  let taskI = await typeormConnection.getRepository(TaskInstance).findOneOrFail(taskInstance.id, {
     relations: ['template', 'template.inputs'],
   })
-  console.log(JSON.stringify(taskI, null, 2))
+  // console.log(JSON.stringify(taskI, null, 2))
 
   if (taskI && taskI.template && taskI.template.inputs) {
     // DataObjectTemplate pro ulohu
@@ -74,15 +160,18 @@ export async function createContext(
       templateId: In([...taskInputsTemplatesIds]),
     })
 
-    console.log('DOI:\n', JSON.stringify(taskInputsInstances, null, 2))
+    // console.log('DOI:\n', JSON.stringify(taskInputsInstances, null, 2))
 
-    let data = createContextInputs({
-      taskInputsInstances,
-      taskInputsTemplates
+    context = createContextForTask({
+      context,
+      taskTemplate: taskI.template,
+      taskInstance: taskI,
+      inputsDataTemplates: taskInputsTemplates,
+      inputsDataInstances: taskInputsInstances,
+      outputsDataTemplates: [],
+      outputsDataInstances: [],
     })
-    context.$INPUT = data
 
-    console.log('DATA:\n', JSON.stringify(data, null, 2))
   }
 
   // console.log(JSON.stringify(context, null, 2))
