956d0afea4bf0d5e7ff667a19694fe97f30c8e19
MWarCZ <mv.deoxys@gmail.com>
2020-02-13 11:43:11 +0100

    Par pokusu s runContext.

--- src/bpmnRunner/runContext.ts
+++ src/bpmnRunner/runContext.ts
@@ -1,11 +1,22 @@
 import { Connection, Equal, In } from 'typeorm'
 
-import { ActivityStatus, DataObjectInstance, DataObjectTemplate, TaskInstance, TaskTemplate } from '../entity/bpmn'
+import {
+  ActivityStatus,
+  DataObjectInstance,
+  DataObjectTemplate,
+  EventInstance,
+  StartEventInstance,
+  StartEventTemplate,
+  TaskInstance,
+  TaskTemplate,
+} from '../entity/bpmn'
 
 ////////////////////////////
 // createXXX - synchroni funkce, ktere nepracuji primo s databazi.
 // loadXXX - asynchroni funkce, ktere pracuji primo s databozi.
 //
+
+//#region Typy
 
 export type RunContextMap = {
   [key: string]: any
@@ -22,13 +33,26 @@ export type RunContextTask = {
   bpmnId: string,
   implementation: string,
 }
+export type RunContextStartEvent = {
+  // Z instance
+  startDate: Date,
+  endData: Date,
+  status: ActivityStatus,
+  // Ze sablony
+  name: string,
+  bpmnId: string,
+}
 
 export type RunContext = {
   $GLOBAL: any,
   $INPUT: RunContextInput,
   $OUTPUT: RunContextOutput,
-  $SELF: Partial<RunContextTask>,
+  $SELF: Partial<RunContextTask> | Partial<RunContextStartEvent>,
 }
+
+//#endregion
+
+//#region Funkce CreateXXX
 
 export function createEmptyContext(): RunContext {
   return {
@@ -131,6 +155,47 @@ export function createContextForTask(
   return context
 }
 
+
+export function createContextForStartEvent(
+  options: {
+    eventTemplate: StartEventTemplate,
+    eventInstance: StartEventInstance,
+    outputsDataTemplates: DataObjectTemplate[],
+    outputsDataInstances: DataObjectInstance[],
+    context?: RunContext,
+  },
+): RunContext {
+  const {
+    eventTemplate,
+    eventInstance,
+    outputsDataTemplates,
+    outputsDataInstances,
+    context = createEmptyContext(),
+  } = options
+
+  context.$SELF = {
+    // Z instance
+    startDate: undefined,
+    endData: undefined,
+    status: eventInstance.status,
+    // Ze sablony
+    bpmnId: eventTemplate.bpmnId,
+    name: eventTemplate.name,
+  }
+
+  let outputsData = createContextOutputs({
+    outputsDataInstances,
+    outputsDataTemplates
+  })
+  context.$OUTPUT = { ...context.$OUTPUT, ...outputsData }
+
+  return context
+}
+
+//#endregion
+
+//#region Funkce LoacXXX
+
 export async function loadContextForTask(
   taskInstance: { id: number },
   typeormConnection: Connection,
@@ -145,9 +210,49 @@ export async function loadContextForTask(
   let context: RunContext = createEmptyContext()
 
   let taskI = await typeormConnection.getRepository(TaskInstance).findOneOrFail(taskInstance.id, {
-    relations: ['template', 'template.inputs'],
+    relations: ['template', 'template.inputs', 'template.outputs'],
   })
   // console.log(JSON.stringify(taskI, null, 2))
+
+  if (taskI && taskI.template) {
+    let inputsDataTemplates: DataObjectTemplate[] = []
+    let inputsDataInstances: DataObjectInstance[] = []
+    let outputsDataTemplates: DataObjectTemplate[] = []
+    let outputsDataInstances: DataObjectInstance[] = []
+
+    if (taskI.template.inputs) {
+    // DataObjectTemplate pro ulohu
+      inputsDataTemplates = taskI.template.inputs
+    // jejich id => DataObjectTemplate.id
+      let inputsDataTemplatesIds = inputsDataTemplates.map(d => d.id)
+      // DataObjectInstance patrici do instance procesu a zaroven do mnoziny vstupu ulohy
+      inputsDataInstances = await typeormConnection.getRepository(DataObjectInstance).find({
+        processInstanceId: Equal(taskI.processInstanceId),
+        templateId: In([...inputsDataTemplatesIds]),
+      })
+    }
+    if (taskI.template.outputs) {
+      // DataObjectTemplate pro ulohu
+      outputsDataTemplates = taskI.template.outputs
+      // jejich id => DataObjectTemplate.id
+      let outputsDataTemplatesIds = outputsDataTemplates.map(d => d.id)
+      // DataObjectInstance patrici do instance procesu a zaroven do mnoziny vstupu ulohy
+      outputsDataInstances = await typeormConnection.getRepository(DataObjectInstance).find({
+        processInstanceId: Equal(taskI.processInstanceId),
+        templateId: In([...outputsDataTemplatesIds]),
+      })
+    }
+
+    context = createContextForTask({
+      context,
+      taskTemplate: taskI.template,
+      taskInstance: taskI,
+      inputsDataTemplates,
+      inputsDataInstances,
+      outputsDataTemplates,
+      outputsDataInstances,
+    })
+  }
 
   if (taskI && taskI.template && taskI.template.inputs) {
     // DataObjectTemplate pro ulohu
@@ -177,3 +282,25 @@ export async function loadContextForTask(
   // console.log(JSON.stringify(context, null, 2))
   return context
 }
+
+export async function loadContextForStartEvent(
+  eventInstance: { id: number },
+  typeormConnection: Connection,
+) {
+  let context: RunContext = createEmptyContext()
+
+  let eventI = await typeormConnection.getRepository(EventInstance).findOneOrFail(eventInstance.id, {
+    relations: ['template'],
+  })
+  if (eventI && eventI.template) {
+    context = createContextForStartEvent({
+      eventTemplate: eventI.template,
+      eventInstance: eventI,
+      outputsDataTemplates: [],
+      outputsDataInstances: [],
+    })
+  }
+  return context
+}
+
+//#endregion
